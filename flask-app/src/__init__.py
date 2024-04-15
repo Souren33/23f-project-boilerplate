c2272721f8d3a4fcc0846c4f6f4febca884bdacb
bpierce31 <benjaminjpierce04@gmail.com>
2024-04-15 13:36:19 -0400

    Added traveler blueprint

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -36,12 +36,14 @@ def create_app():
     from src.customers.customers import customers
     from src.products.products  import products
     from src.owners import owners   
+    from src.travelers import travelers
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(owners, url_prefix='/o')
+    app.register_blueprint(travelers, url_prefix='/t')
 
     # Don't forget to return the app object
     return app
--- flask-app/src/owners.py
+++ flask-app/src/owners.py
@@ -6,26 +6,13 @@ from src import db
 
 owners = Blueprint('owners', __name__)
 
-# Get all the properties from the database
-@owners.route('/properties', methods=['GET'])
-def get_properties():
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Property')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
 
-# Get property detail for property with particular propertyID
-@owners.route('/properties/<propertyID>', methods=['GET'])
-def get_property(propertyID):
+
+# retrieve all properties of an owner
+@owners.route('/properties/<ownerID>', methods=['GET'])
+def get_properties(ownerID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from Property where propertyID = {0}'.format(propertyID))
+    cursor.execute('select * from Property where propertyID = {0}'.format(ownerID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
