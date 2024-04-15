790478437b818e344687f550a825a6786440cea1
bpierce31 <benjaminjpierce04@gmail.com>
2024-04-15 18:28:25 -0400

    Added first POST route in owners

--- flask-app/src/owners.py
+++ flask-app/src/owners.py
@@ -142,3 +142,43 @@ def get_stay_at(propertyID, travelerID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# create a new property
+@owners.route('/createproperty', methods=['POST'])
+def add_new_property():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    address = the_data['address']
+    city = the_data['region']
+    region = the_data['region']
+    country = the_data['country']
+    type = the_data['type']
+    description = the_data['description']
+    title = the_data['title']
+    price = the_data['price']
+    ownerID = the_data['ownerID']
+
+    # Constructing the query
+    query = 'insert into Property (address, city, region, country, type, description, title, price, ownerID) values ("'
+    query += address + '", "'
+    query += city + '", "'
+    query += region + '", '
+    query += country + '", "'
+    query += type + '", "'
+    query += description + '", '
+    query += title + '", '
+    query += str(price) + '", '
+    query += ownerID + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
