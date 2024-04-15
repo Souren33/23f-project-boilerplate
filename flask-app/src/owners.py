82e6793a5b6ed6288db58f25773a7570a064bbbf
bpierce31 <benjaminjpierce04@gmail.com>
2024-04-15 17:47:45 -0400

    Fixed initial owner route, added second route

--- flask-app/src/owners.py
+++ flask-app/src/owners.py
@@ -12,7 +12,22 @@ owners = Blueprint('owners', __name__)
 @owners.route('/properties/<ownerID>', methods=['GET'])
 def get_properties(ownerID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from Property where propertyID = {0}'.format(ownerID))
+    cursor.execute('select * from Property where ownerID = {0}'.format(ownerID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# retrieve information on one property of an owner
+@owners.route('/properties/<ownerID>/<propertyID>', methods=['GET'])
+def get_property(ownerID, propertyID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Property where ownerID = {0} AND propertyID = {1}'.format(ownerID, propertyID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
