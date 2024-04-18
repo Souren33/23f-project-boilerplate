1bfa194fff66f35b754bfa8c86c853a953829555
Souren33 <prakashsouren@gmail.com>
2024-04-18 18:49:59 -0400

    Adding fixes to advertisers

--- flask-app/src/advertisers.py
+++ flask-app/src/advertisers.py
@@ -4,7 +4,12 @@ from src import db
 
 
 
+
+
+
 advertisers = Blueprint('advertisers', __name__)
+
+
 
 
 @advertisers.route('/advertisersinfo/<advertiserID>', methods=['GET'])
@@ -21,12 +26,13 @@ def get_advertiserinfo(advertiserID):
     the_response.mimetype = 'application/json'
     return the_response
 
+
 # GET routes
 # View the liaison information for a specifc advertiser
-@advertisers.route('/AdLiaisons/<liaisonID>', methods=['GET'])
-def get_liason(liaisonID):
+@advertisers.route('/AdLiaisons/<adID>', methods=['GET'])
+def get_liason(adID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from AdLiaison where liaisonID = {0}'.format(liaisonID))
+    cursor.execute('SELECT * FROM AdLiaison AD JOIN Advertiser A ON AD.liaisonID = A.liaisonID WHERE AD.liaisonID = {0}'.format(adID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -38,11 +44,13 @@ def get_liason(liaisonID):
     return the_response
 
 
-#Allow advertiser to view traveler data 
-@advertisers.route('/TravelData/<advertiserID>', methods=['GET'])
+
+
+#Allow advertiser to view traveler data
+@advertisers.route('/Travel_Data/<advertiserID>', methods=['GET'])
 def get_traveler_data(advertiserID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from TravelData TD JOIN Traveler T ON TD.travelerID = t.travelerID where advertiserID = {0}'.format(advertiserID))
+    cursor.execute('select * from Travel_Data TD JOIN Traveler T ON TD.travelerID = T.travelerID where advertiserID = {0}'.format(advertiserID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -52,15 +60,19 @@ def get_traveler_data(advertiserID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+
+
 
 
 
 
 #Allow advertiser to view property data
-@advertisers.route('/PropertyData/<advertiserID>', methods=['GET'])
+@advertisers.route('/Property_Data/<advertiserID>', methods=['GET'])
 def get_property_data(advertiserID):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from TravelData TD JOIN Property P ON TD.travelerID = P.propertyID where advertiserID = {0}'.format(advertiserID))
+    cursor.execute('select * from Property_Data PD JOIN Property P ON PD.propertyID = P.propertyID where advertiserID = {0}'.format(advertiserID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -73,24 +85,10 @@ def get_property_data(advertiserID):
 
 
 
-# Get property detail for property with particular propertyID
-@advertisers.route('/properties/<propertyID>', methods=['GET'])
-def get_property(propertyID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from Property where propertyID = {0}'.format(propertyID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
 
 
 
-#views all expereince ads by a specific advertiser 
+#views all expereince ads by a specific advertiser
 @advertisers.route('/ExperienceAds/<advertiserID>', methods=['GET'])
 def get_ad_info(advertiserID):
     cursor = db.get_db().cursor()
@@ -104,3 +102,4 @@ def get_ad_info(advertiserID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
